5b56cff8eae29891d1683b46724ec2343ecd5765
20IT120-JIMMY <20it120@charusat.edu.in>
2023-10-04 12:23:48 +0530

    projects > image size fix , card border readius fix , hide horizantal scrollbar ---> Done ...

--- react-1-me/src/components/Main/Mainheader/Pagenavigartion/PageDetails/Pagedetail.jsx
+++ react-1-me/src/components/Main/Mainheader/Pagenavigartion/PageDetails/Pagedetail.jsx
@@ -12,149 +12,178 @@ import other2 from "../../../../../assets/profiles/other2.jpg";
 import other3 from "../../../../../assets/profiles/other3.jpg";
 
 function Pagedetail() {
-  const Newreq = [
+  const requestTypes = [
     {
-      priotitle: "High",
-      title: "Hero Section",
-      sdate: "20 May, 2023",
-      edate: "24 May, 2023",
-      img: img1,
-      desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
-      subtask: 5,
-      attachment: 3,
+      title: "New request",
+      data: [
+        {
+          priotitle: "High",
+          title: "Hero Section",
+          sdate: "20 May, 2023",
+          edate: "24 May, 2023",
+          img: img1,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 5,
+          attachment: 3,
+        },
+        {
+          priotitle: "Low",
+          title: "Knowledge Center",
+          sdate: "21 May, 2023",
+          edate: "25 May, 2023",
+          img: img3,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 1,
+          attachment: 4,
+          cardimg: other1,
+        },
+        {
+          priotitle: "Medium",
+          title: "Careers",
+          sdate: "21 May, 2023",
+          edate: "25 May, 2023",
+          img: img2,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 5,
+          attachment: 2,
+        },
+      ],
     },
     {
-      priotitle: "Low",
-      title: "Knowledge Center",
-      sdate: "21 May, 2023",
-      edate: "25 May, 2023",
-      img: img3,
-      desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
-      subtask: 1,
-      attachment: 4,
-      cardimg: other1,
+      title: "In progress",
+      data: [
+        {
+          priotitle: "High",
+          title: "About us section",
+          sdate: "20 May, 2023",
+          edate: "24 May, 2023",
+          img: img4,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 5,
+          attachment: 3,
+        },
+        {
+          priotitle: "Medium",
+          title: "Legal Notice",
+          sdate: "21 May, 2023",
+          edate: "25 May, 2023",
+          img: img5,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 5,
+          attachment: 2,
+          cardimg: other2,
+        },
+        {
+          priotitle: "Low",
+          title: "Knowledge Center",
+          sdate: "21 May, 2023",
+          edate: "25 May, 2023",
+          img: img3,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 1,
+          attachment: 4,
+          cardimg: other1,
+        },
+      ],
     },
     {
-      priotitle: "Medium",
-      title: "Careers",
-      sdate: "21 May, 2023",
-      edate: "25 May, 2023",
-      img: img2,
-      desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
-      subtask: 5,
-      attachment: 2,
-    },
-  ];
-  const Progreq = [
-    {
-      priotitle: "High",
-      title: "About us section",
-      sdate: "20 May, 2023",
-      edate: "24 May, 2023",
-      img: img4,
-      desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
-      subtask: 5,
-      attachment: 3,
-    },
-    {
-      priotitle: "Medium",
-      title: "Leegal Notice",
-      sdate: "21 May, 2023",
-      edate: "25 May, 2023",
-      img: img5,
-      desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
-      subtask: 5,
-      attachment: 2,
-      cardimg: other2,
-    },
-    {
-      priotitle: "Low",
-      title: "Knowledge Center",
-      sdate: "21 May, 2023",
-      edate: "25 May, 2023",
-      img: img3,
-      desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
-      subtask: 1,
-      attachment: 4,
-      cardimg: other1,
-    },
-  ];
-  const Needreq = [
-    {
-      priotitle: "High",
-      title: "Discover Section",
-      sdate: "20 May, 2023",
-      edate: "24 May, 2023",
-      img: img1,
-      desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
-      subtask: 5,
-      attachment: 3,
-      cardimg: other3,
+      title: "Need Review",
+      data: [
+        {
+          priotitle: "High",
+          title: "Discover Section",
+          sdate: "20 May, 2023",
+          edate: "24 May, 2023",
+          img: img1,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 5,
+          attachment: 3,
+          cardimg: other3,
+        },
+        {
+          priotitle: "Low",
+          title: "Provider",
+          sdate: "21 May, 2023",
+          edate: "25 May, 2023",
+          img: img2,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 5,
+          attachment: 2,
+        },
+        {
+          priotitle: "Medium",
+          title: "Knowledge Center",
+          sdate: "21 May, 2023",
+          edate: "25 May, 2023",
+          img: img3,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 1,
+          attachment: 4,
+        },
+      ],
     },
     {
-      priotitle: "Low",
-      title: "Provider",
-      sdate: "21 May, 2023",
-      edate: "25 May, 2023",
-      img: img2,
-      desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
-      subtask: 5,
-      attachment: 2,
+      title: "Done",
+      data: [
+        {
+          priotitle: "High",
+          title: "Purchase, finance, service section",
+          sdate: "20 May, 2023",
+          edate: "24 May, 2023",
+          img: img1,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 5,
+          attachment: 3,
+        },
+        {
+          priotitle: "Low",
+          title: "Knowledge Center",
+          sdate: "21 May, 2023",
+          edate: "25 May, 2023",
+          img: img6,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 1,
+          attachment: 4,
+        },
+      ],
     },
     {
-      priotitle: "Medium",
-      title: "Knowledge Center",
-      sdate: "21 May, 2023",
-      edate: "25 May, 2023",
-      img: img3,
-      desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
-      subtask: 1,
-      attachment: 4,
-    },
-  ];
-  const Donereq = [
-    {
-      priotitle: "High",
-      title: "Purchase, finance, service section",
-      sdate: "20 May, 2023",
-      edate: "24 May, 2023",
-      img: img1,
-      desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
-      subtask: 5,
-      attachment: 3,
-    },
-    {
-      priotitle: "Low",
-      title: "Knowledge Center",
-      sdate: "21 May, 2023",
-      edate: "25 May, 2023",
-      img: img6,
-      desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etcthe main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
-      subtask: 1,
-      attachment: 4,
+      title: "Done",
+      data: [
+        {
+          priotitle: "High",
+          title: "Purchase, finance, service section",
+          sdate: "20 May, 2023",
+          edate: "24 May, 2023",
+          img: img1,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 5,
+          attachment: 3,
+        },
+        {
+          priotitle: "Low",
+          title: "Knowledge Center",
+          sdate: "21 May, 2023",
+          edate: "25 May, 2023",
+          img: img6,
+          desc: "the main written part of a book, newspaper, etc. not the pictures, notes, index, etc",
+          subtask: 1,
+          attachment: 4,
+        },
+      ],
     },
   ];
 
   return (
     <div
       className="pagedetails"
-      style={{ "grid-template-columns": "repeat(5, 30rem)" }}
+      style={{ gridTemplateColumns: "repeat(5, 30rem)" }}
     >
-      <div className="req-type">
-        <Requesttype reqtitle={"New request"} data={Newreq} />
-      </div>
-      <div className="req-type">
-        <Requesttype reqtitle={"In progress"} data={Progreq} />
-      </div>
-      <div className="req-type">
-        <Requesttype reqtitle={"Need Review"} data={Needreq} />
-      </div>
-      <div className="req-type">
-        <Requesttype reqtitle={"Done"} data={Donereq} />
-      </div>
-      <div className="req-type">
-        <Requesttype reqtitle={"Done"} data={Donereq} />
-      </div>
+      {requestTypes.map((requestType, index) => (
+        <div className="req-type" key={index}>
+          <Requesttype reqtitle={requestType.title} data={requestType.data} />
+        </div>
+      ))}
     </div>
   );
 }
