7be93b2ccac90d59be5d6a41b2525898c169e409
20IT120-JIMMY <20it120@charusat.edu.in>
2023-10-04 10:58:36 +0530

    sideber > project - dropdown added ...

--- react-1-me/src/components/Sidebar/ProjSelection/ProjSelection.jsx
+++ react-1-me/src/components/Sidebar/ProjSelection/ProjSelection.jsx
@@ -1,24 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProjSelection.css";
 
 function ProjSelection() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const dropdownItems = [
+    {
+      logo: "beer",
+      title: "Hatypo Projects",
+      member: "11 members",
+    },
+    {
+      logo: "logo-behance",
+      title: "Another Projects",
+      member: "10 members",
+    },
+  ];
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleDropdownItemClick = (item) => {
+    setSelectedItem(item);
+    setIsDropdownOpen(false);
+  };
+
   return (
-    <div className="proj-selec-container">
-      <div className="proj-logo">
-        <ion-icon name="beer" className="beer-icon"></ion-icon>
-      </div>
-      <div className="proj-title-member-container">
-        <div className="proj-title-container">
-          <p className="proj-title">Hatypo Projects</p>
+    <div className="proj-selec-container" onClick={toggleDropdown}>
+      <div className="drop-item">
+        <div className="proj-logo">
           <ion-icon
-            name="chevron-down-outline"
-            className="dropdown-icon"
+            name={selectedItem ? selectedItem.logo : "beer"}
+            className="beer-icon"
           ></ion-icon>
         </div>
-        <div className="proj-member-container">
-          <p className="proj-member">11 member</p>
+        <div className="proj-title-member-container">
+          <div className="proj-title-container">
+            <p className="proj-title">
+              {selectedItem ? selectedItem.title : "Hatypo Projects"}
+            </p>
+            <ion-icon
+              name="chevron-down-outline"
+              className="dropdown-icon"
+            ></ion-icon>
+          </div>
+          <div className="proj-member-container">
+            <p className="proj-member">
+              {selectedItem ? selectedItem.member : "11 members"}
+            </p>
+          </div>
         </div>
       </div>
+
+      {isDropdownOpen && (
+        <div className="dropdown-content">
+          {dropdownItems.map((item, index) => (
+            <div
+              key={index}
+              className="dropdown-item"
+              onClick={() => handleDropdownItemClick(item)}
+            >
+              <div className="proj-logo">
+                <ion-icon name={item.logo} className="beer-icon"></ion-icon>
+              </div>
+              {item.title}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
